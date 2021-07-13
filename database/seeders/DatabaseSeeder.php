462ec7549ad8d7787aaa4f3ee0047104e148ad36
TomF46 <t-d-f@live.co.uk>
2021-07-13 12:13:38 +0100

    Improve seed to create some locations for quicker testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Enums\Roles;
 use App\Models\User;
+use App\Models\Area;
+use App\Models\RetailLocation;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,5 +25,31 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt(env('ADMIN_PASSWORD')),
             'role' => Roles::Administrator
         ]);
+
+        $headOffice = User::factory()->create([
+            'email' => 'headOffice@email.com',
+            'password' => bcrypt(env('ADMIN_PASSWORD')),
+            'role' => Roles::HeadOffice
+        ]);
+
+        $areaManager = User::factory()->create([
+            'email' => 'areaManager@email.com',
+            'password' => bcrypt(env('ADMIN_PASSWORD')),
+            'role' => Roles::AreaManager
+        ]);
+
+        $area = Area::factory()->create();
+        $area->managers()->save($areaManager);
+
+        $retailManager = User::factory()->create([
+            'email' => 'retailManager@email.com',
+            'password' => bcrypt(env('ADMIN_PASSWORD')),
+            'role' => Roles::RetailManager
+        ]);
+
+        $location = RetailLocation::factory()->create([
+            'area_id' => $area->id
+        ]);
+        $location->managers()->save($retailManager);
     }
 }
