64118a40aca144e1ab3bb7038e7d1a45838e6f6a
TomF46 <t-d-f@live.co.uk>
2023-10-25 11:15:04 +0100

    Front end filter on priority and status for a given year.

---
+++ resources/js/components/Dashboard/HeadOfficeDashboard/Years/Applications/ApplicationsSearch.js
@@ -0,0 +1,85 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { getAreas } from "../../../../../api/areasApi";
+import SelectInput from "../../../../FormComponents/SelectInput";
+
+const ApplicationsSearch = ({ filters,  onUpdateFilters}) => {
+    // const [areas, setAreas] = useState(null);
+
+    const prioritOptions = [
+        {text: "Low", value: 0},
+        {text: "Medium", value: 1},
+        {text: "High", value: 2},
+        {text: "Severe", value: 3}
+    ];
+
+    const statusOptions = [
+        {text: "Not submitted", value: 0},
+        {text: "Submitted", value: 1},
+        {text: "Returned", value: 2},
+        {text: "Accepted", value: 3},
+        {text: "Inactive", value: 4},
+    ];
+
+    // useEffect(()=>{
+    //     getAreas().then(data => {
+    //         setAreas(data);
+    //     })
+    // }, []);
+
+    function onChange(event){
+        const { name, value } = event.target;
+        let input = value;
+
+        input = name == ("priority" || name == "status") && value == "All" ? null : Number(input);
+
+        let copy = structuredClone(filters);
+        console.log(input);
+        onUpdateFilters({...copy, [name]: input});
+    }
+
+    return (
+        <div className="my-4 card shadow-md rounded-md">
+            <div className="bg-secondary rounded-t-md">
+                <p className="text-white font-bold text-lg px-2 py-1">Search</p>
+            </div>
+            <div className="px-2 py-4 flex">
+                {/* <SelectInput
+                    name="area"
+                    label="Area"
+                    value={filters.area}
+                    options={areas}
+                    onChange={onChange}
+                    error={errors.area_id}
+                /> */}
+                <div>
+                    <SelectInput
+                        name="status"
+                        label="Status"
+                        value={filters.status}
+                        defaultOption={'All'}
+                        options={statusOptions}
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="mx-2">
+                    <SelectInput
+                        name="priority"
+                        label="Priority"
+                        value={filters.priority}
+                        defaultOption={'All'}
+                        options={prioritOptions}
+                        onChange={onChange}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ApplicationsSearch.propTypes = {
+    filters: PropTypes.object.isRequired,
+    onUpdateFilters: PropTypes.func.isRequired
+};
+
+export default ApplicationsSearch;
--- resources/js/components/Dashboard/HeadOfficeDashboard/Years/Applications/YearApplicationsPage.js
+++ resources/js/components/Dashboard/HeadOfficeDashboard/Years/Applications/YearApplicationsPage.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { getYearById } from "../../../../../api/yearsApi";
 import LoadingMessage from "../../../../DisplayComponents/LoadingMessage";
-import { getPaginationPage, searchApplications } from "../../../../../api/applicationsApi";
+import { getPageWithPaginationUrlAndFilters, getPaginationPage, searchApplications } from "../../../../../api/applicationsApi";
 import { useParams } from "react-router-dom";
 import HeadOfficeApplicationsListWithPagination from "../../../../DisplayComponents/HeadOfficeApplicationsListWithPagination";
+import ApplicationsSearch from "./ApplicationsSearch";
+import { debounce } from "lodash";
 
 const YearApplicationsPage = () => {
     const { yearId } = useParams();
     const [year, setYear] = useState(null);
     const [applicationsPaginator, setApplicationsPaginator] = useState(null);
-    const [filters, setFilters] = useState({ year: yearId});
+    const [filters, setFilters] = useState({ year: yearId, status: null, priority: null});
 
     useEffect(() => {
         if (!year) {
@@ -20,6 +22,14 @@ const YearApplicationsPage = () => {
             getApplications();
         }
     }, [yearId, year])
+
+    useEffect(() => {
+        let debounced = debounce(
+            () => { getApplications(); }, 50
+        );
+
+        debounced();
+    }, [filters]);
 
     function getYear() {
         getYearById(yearId).then(yearData => {
@@ -42,7 +52,7 @@ const YearApplicationsPage = () => {
     }
 
     function getPage(url){
-        getPaginationPage(url).then(applicationData => {
+        getPageWithPaginationUrlAndFilters(url, filters).then(applicationData => {
             setApplicationsPaginator(applicationData);
         }).catch(error => {
             toast.error("Error getting applications for year " + error.message, {
@@ -61,8 +71,10 @@ const YearApplicationsPage = () => {
                         {year.year} applications
                     </h1>
 
+                    <ApplicationsSearch filters={filters} onUpdateFilters={(newFilters) => setFilters(newFilters)} />
+
                     {!applicationsPaginator ? (
-                        <LoadingMessage message={"Loading completed applications."} />
+                        <LoadingMessage message={"Loading applications."} />
                     ) : (
                         <div className="card shadow-md rounded-md">
                             <div className="bg-primary rounded-t-md">
@@ -72,7 +84,7 @@ const YearApplicationsPage = () => {
                                 {applicationsPaginator.total > 0 ? (
                                     <HeadOfficeApplicationsListWithPagination paginationData={applicationsPaginator} onPageChange={getPage}/>
                                 ) : (
-                                    <p className="text-center p-4">This year has no completed applications.</p>
+                                    <p className="text-center p-4">This year has no applications that match your search.</p>
                                 )}
                             </div>
                         </div>
